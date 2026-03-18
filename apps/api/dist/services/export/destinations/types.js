4d447df275ef9f6308519fb1fb093218bea5aa36
oceanwonders42-crypto <oceanwonders42@gmail.com>
2026-03-18 11:38:54 -0400

    Stop tracking compiled API dist output

--- apps/api/dist/services/export/destinations/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Export destination kinds and result contract.
- * New CRM adapters plug in by implementing IExportDestination for kind "crm".
- */
-Object.defineProperty(exports, "__esModule", { value: true });
